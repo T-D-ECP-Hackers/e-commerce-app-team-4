c2afa6f1f46fc0fc972108f7af160c16ffb777ba
Gus Lawrence <gus.lawrence@global.com>
2023-06-01 10:25:45 +0100

    feat: backend work part 1

--- backend/src/main/java/org/global/ecp/hackathon/app/basket/BasketService.java
+++ backend/src/main/java/org/global/ecp/hackathon/app/basket/BasketService.java
@@ -24,24 +24,52 @@ public class BasketService {
 
     // TODO - Task 3: add basketProduct to basket using the basketRepository
     // Hint: basketRepository.someMethod(variableToPassIntoMethod);
-    public Basket addToBasket(final Long productId) {
+public Basket addToBasket(final Long productId) {
+    Basket basket = getBasket();
+    BasketProduct basketProduct = basketProductService.createBasketProduct(basket, productId);
+    basketProduct.increaseQuantity();
 
-        BasketProduct basketProduct = basketProductService.createBasketProduct(getBasket(), productId);
-        basketProduct.increaseQuantity();
-        return null;
-    }
+    // Add the basketProduct to the basket
+    basket.getProducts().add(basketProduct);
+
+    // Save the updated basket using the basketRepository
+    basketRepository.save(basket);
+
+    return basket;
+}
 
     // TODO - Task 5: remove basketProduct from the basket using the basketRepository
-    public Basket removeFromBasket(final Long productId) {
+public Basket removeFromBasket(final Long productId) {
+    Basket basket = getBasket();
 
-        BasketProduct basketProduct = basketProductService.createBasketProduct(getBasket(), productId);
-        basketProduct.decreaseQuantity();
-        return null;
-    }
+    // Retrieve the BasketProduct from the Basket using the productId
+    BasketProduct basketProduct = basketProductService.createBasketProduct(basket, productId);
+
+    // Decrease the quantity of the BasketProduct
+    basketProduct.decreaseQuantity();
+
+    // Remove the BasketProduct from the Basket's product list
+    basket.getProducts().remove(basketProduct);
+
+    // Save the updated basket using the basketRepository
+    basketRepository.save(basket);
+
+    return basket;
+}
 
     // TODO - Task 8: implement checkout method
-    public Basket checkout() {
+public Basket checkout() {
+    Basket basket = getBasket();
 
-        return null;
-    }
+    // Create an order or transaction object
+    Order order = createOrderFromBasket(basket); // You need to define the createOrderFromBasket() method
+
+    // Save the order or transaction to the database
+    orderRepository.save(order); // Assuming you have an appropriate orderRepository
+
+    // Clear the basket (remove all BasketProduct instances or reset to an empty state)
+    basket.clearProducts();
+
+    // Return the updated Basket or any other relevant information
+    return basket;
 }
